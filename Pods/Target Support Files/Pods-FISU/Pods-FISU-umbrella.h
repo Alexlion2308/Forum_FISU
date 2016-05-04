9574cea6dcf8a23d8c157eecc5e1bc52a1fec798
licettea <AurelienL@imac-de-aurelien.local>
2016-05-04 14:17:59 +0200

    pod

--- Pods/Target Support Files/Pods-FISU/Pods-FISU-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_FISUVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_FISUVersionString[];
-
--- Pods/Target Support Files/Pods-FISUUITests/Pods-FISUUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FISUUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FISUUITests
-@end
