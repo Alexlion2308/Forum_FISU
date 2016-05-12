19612df18d0fc50e9ef466ca2c4ad549be9e3a31
RedaMaachiGit <maachi.reda@gmail.com>
2016-05-12 16:32:48 +0200

    app delegate version

--- FISU/AppDelegate.swift
+++ FISU/AppDelegate.swift
@@ -14,12 +14,12 @@ import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     
     var window: UIWindow?
     
     //Initialisation de l'ensemble de données destinées à être stockées dans le coreData
-
+    
     var eventsDay1 : [[String]] = [["09:00-12:30","Opening ceremony", "Welcoming ceremony, presentation of the activities schedule and of the speakers", "None", "image1","Timberlake","Justin","Corum"], ["12:30-13:45","Lunch", "Eating session", "La catégorie des branlos", "image2","None","None","Maison Relations Internationales (MRI)"], ["14:15-15:15","Introduction to the week's aims/methodology", "Introduction to the week's aims/methodology", "Coaching", "image3","Timberlake","Justin","Institut de Botanique"]]
     
     var eventsDay2 : [[String]] = [["09:00-10:00","Plenary session: Values/Ethics/Integrity", "Plenary session: Values/Ethics/Integrity", "Social", "image1","Timberlake","Justin","Institut de Botanique"], ["10:15-11:15","Workshops: Values/Ethics/Integrity", "Workshops:  Values/Ethics/Integrity", "Social", "image2","Timberlake","Justin","Saint Charles"], ["11:15-11:30","Coffee break","Break","None","image3","Timberlake","Justin","Saint Charles"]]
@@ -32,12 +32,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var eventsDay6 : [[String]] = [["09:00-11:00","Project presentation", "Presentation", "None", "image1","Timberlake","Justin","Institut de Botanique"], ["11:00-11:30","Deliberation", "Jury deliberation and outcome announcement", "None", "image2","Timberlake","Justin","Institut de Botanique"], ["11:30-12:30","Forum conclusions", "Forum conclusions", "None", "image3", "Timberlake","Justin","Institut de Botanique"]]
     
-
+    
     var daySet : [String] = ["DAY 1: Monday, July 4", "DAY 2: Tuesday, July 5", "DAY 3: Wednesday, July 6", "DAY 4: Thursday, July 7", "DAY 5: Friday, July 8", "DAY 6: Saturday, July 9"]
-
+    
     var placeSet : [[String]] = [["Corum","1","Boulevard Charles Warnery","34000","Montpellier","43.61377","3.88225"], ["Institut de Botanique","163","Rue Auguste Broussonnet","34090","Montpellier","43.61606","3.87161"], ["Maison Relations Internationales (MRI)","14","Descente en Barrat","34000","Montpellier","43.61305","3.87831"], ["Saint Charles","1500","Route de Mende","34100","Montpellier","43.63210","3.86836"], ["UFR STAPS","700","Avenue du Pic Saint-Loup","34090", "Montpellier","43.64108","3.85117"], ["Veyrassi","700","Avenue du Pic Saint-Loup","34090", "Montpellier","43.64108","3.85117"]]
-
-    var speakerSet : [[String]]?
+    
+    var speakerSet : [[String]] = [["None","None","None","None","None","None"],["Justin", "Timberlake", "Singer", "Belge", "This willfull man has beady chestnut eyes.", "profilePicture2"], ["Spparow", "Jack", "Acteur", "Belge", "Une belle description de la personne concernée", "profilePicture2"], ["Nous", "Prenom", "Pompiste", "Belge", "description", "profilePicture3"], ["Lacha", "Bernard", "Pompiste", "Belge", "description", "profilePicture4"], ["Lui", "Prenom", "Pompiste", "Belge", "description", "profilePicture5"] ,["Elle", "Prenom", "Pompiste", "Belge", "description", "profilePicture4"], ["Belge", "Belge", "Belge", "Belge", "Belge", "profilePicture3"]]
     
     var usersSet : [[String]] = [["reda", "reda"],["aurelien", "aurelien"]]
     
@@ -56,17 +56,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         GMSServices.provideAPIKey("AIzaSyAEtmY3vdAP89Si76FsqSjfb7VjLDYu3bQ")
         //////////////////////// Test Alamofire //////////////////////////
-
+        
         Alamofire.request(.GET, "https://fisuwebfinal-madonna.rhcloud.com/ListeEvent.php")
             .responseJSON { response in
                 /*print("Requete: ")
-                print(response.request)  // original URL request
-                print("Reponse: ")
-                print(response.response) // URL response
-                print("Data: ")
-                print(response.data)  // server data
-                print("Result: ")
-                print(response.result)   // result of response serialization*/
+                 print(response.request)  // original URL request
+                 print("Reponse: ")
+                 print(response.response) // URL response
+                 print("Data: ")
+                 print(response.data)  // server data
+                 print("Result: ")
+                 print(response.result)   // result of response serialization*/
                 
                 if let obj = response.result.value { // Je récupere le json de la page
                     let jsonEvent = JSON(obj)
@@ -88,11 +88,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                         print(attributSpeaker["EmailSpeaker"])
                                         print(attributSpeaker["NationaliteSpeaker"])
                                         print(attributSpeaker["DescSpeaker"])
-                                        let oneSpeakerTable: [String] = [(attributSpeaker["NomSpeaker"]).toString(),(attributSpeaker["PrenomSpeaker"]).toString(),(attributSpeaker["Profession"]).toString(),(attributSpeaker["EmailSpeaker"]).toString(),(attributSpeaker["NationaliteSpeaker"]).toString(),(attributSpeaker["DescSpeaker"]).toString()]
-                                        
-                                        self.speakerSet.append(oneSpeakerTable)
-                                        print(self.speakerSet)
-                                        Fisu.createSpeakers(self.speakerSet)
                                     }
                                 }
                                 //print("StrB: " + b.toString())
@@ -103,17 +98,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     }
                 }
         }
-
+        
         //////////////////////// APPLICATION INIT //////////////////////////
         
         //Call de toutes les fonctions de création du coreData
-
+        
         User.createUsers(usersSet[0])
         
         Fisu.createRestaurants(restaurantSet)
         
+        Fisu.createSpeakers(speakerSet)
+        
         Fisu.createPlaces(placeSet)
-    
+        
         Fisu.createDays(daySet,eventSet1: eventsDay1,eventSet2: eventsDay2,eventSet3: eventsDay3,eventSet4: eventsDay4,eventSet5: eventsDay5,eventSet6: eventsDay6)
         
         
@@ -124,49 +121,49 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             print("Could not save \(error), \(error.userInfo)")
         }
         
-
+        
         
         return true
     }
-
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
         self.saveContext()
     }
-
+    
     // MARK: - Core Data stack
-
+    
     lazy var applicationDocumentsDirectory: NSURL = {
         // The directory the application uses to store the Core Data store file. This code uses a directory named "IG4-POLYTECH.FISU" in the application's documents Application Support directory.
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         return urls[urls.count-1]
     }()
-
+    
     lazy var managedObjectModel: NSManagedObjectModel = {
         // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
         let modelURL = NSBundle.mainBundle().URLForResource("FISU", withExtension: "momd")!
         return NSManagedObjectModel(contentsOfURL: modelURL)!
     }()
-
+    
     lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
         // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
         // Create the coordinator and store
@@ -180,7 +177,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             var dict = [String: AnyObject]()
             dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
             dict[NSLocalizedFailureReasonErrorKey] = failureReason
-
+            
             dict[NSUnderlyingErrorKey] = error as NSError
             let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
             // Replace this with code to handle the error appropriately.
@@ -191,7 +188,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return coordinator
     }()
-
+    
     lazy var managedObjectContext: NSManagedObjectContext = {
         // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
         let coordinator = self.persistentStoreCoordinator
@@ -199,9 +196,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         managedObjectContext.persistentStoreCoordinator = coordinator
         return managedObjectContext
     }()
-
+    
     // MARK: - Core Data Saving support
-
+    
     func saveContext () {
         if managedObjectContext.hasChanges {
             do {
@@ -215,6 +212,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
+    
 }
-
