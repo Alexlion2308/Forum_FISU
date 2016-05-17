2ba210549aecb002111e2470a050debe48eee700
RedaMaachiGit <maachi.reda@gmail.com>
2016-05-17 19:02:26 +0200

    speakers events places

--- FISU/PlaceViewController.swift
+++ FISU/PlaceViewController.swift
@@ -11,7 +11,7 @@ import MapKit
 
 class PlaceViewController: UIViewController {
     
-    var placeSelected : Place?
+    var jsonPlaces: JSON? = nil
     
     @IBOutlet weak var mapView: MKMapView!
 
@@ -26,52 +26,45 @@ class PlaceViewController: UIViewController {
         
         mapView.delegate = self
         
-        guard let place = self.placeSelected else{
-            return
-        }
-        guard let placeNom = place.nom else {
-            return
-        }
-        guard let placeNumRue = place.numRue else {
-            return
-        }
-        guard let placeNomRue = place.nomRue else {
-            return
-        }
-        guard let placeCP = place.codePostal else {
-            return
-        }
-        guard let placeVille = place.ville else {
-            return
-        }
-        guard let placeLatitude = place.latitude else {
-            return
-        }
-        guard let placeLongitude = place.longitude else {
-            return
-        }
-        
-        self.labelNomPlace.text = placeNom
-        self.labelRuePlace.text = placeNumRue+" "+placeNomRue
-        self.labelVille.text = placeCP+" "+placeVille
-        
-        
 
+        guard let jsonPlacesToLoop = self.jsonPlaces else{
+            print("guard jsonEventsToLoop")
+            return
+        }
+        for (key, places) in jsonPlacesToLoop { // cle is NSNumber, event is another JSON object (event c'est chaque event)
+            //print("Numéro con: " + events["numeroEvent"].toString()) // TRACE
+            //print("Selectionné: " + String(selection)) // TRACE
+                guard let latitude = Double(places["LatitudePlace"].toString()) else{
+                    print("pas de latitude guard")
+                    return
+                }
+                guard let longitude = Double(places["LongitudePlace"].toString()) else{
+                    print("pas de longitude guard")
+                    return
+                }
+                let nomPlace = places["NomPlace"].toString()
+                let ruePlace = places["NomRuePlace"].toString()
+                let numRuePlace = places["NumRuePlace"].toString()
+                let villePlace = places["VillePlace"].toString()
 
-        // Initialisation de la map en affichant la zone correspondant à la latitude et la longitude de l'endroit
-        let initialLocation = CLLocation(latitude: Double(placeLatitude), longitude: Double(placeLongitude))
-        
-        //Solicitation de la fonction centerMapOnLocation afin d'effectuer un zoom sur la zone correspondante
-        centerMapOnLocation(initialLocation)
-        
-        //Création du pin affichant avec précision l'endroit sélectionné, nom plus adresse renseigné permettant d'afficher ces informations lorsque l'utilisateur clique sur le pin
-        let artwork = Artwork(title: placeNom,
-            locationName: placeNumRue+" "+placeNomRue,
-            coordinate: CLLocationCoordinate2D(latitude: Double(placeLatitude), longitude: Double(placeLongitude)))
-        
-        //Ajout du pin
-        mapView.addAnnotation(artwork)
-        
+                self.labelNomPlace.text = nomPlace
+                self.labelRuePlace.text = ruePlace
+                self.labelVille.text = villePlace
+
+                // Initialisation de la map en affichant la zone correspondant à la latitude et la longitude de l'endroit
+                let initialLocation = CLLocation(latitude: latitude, longitude: longitude)
+                
+                //Solicitation de la fonction centerMapOnLocation afin d'effectuer un zoom sur la zone correspondante
+                centerMapOnLocation(initialLocation)
+                
+                //Création du pin affichant avec précision l'endroit sélectionné, nom plus adresse renseigné permettant d'afficher ces informations lorsque l'utilisateur clique sur le pin
+                let artwork = Artwork(title: nomPlace,
+                                      locationName: numRuePlace+" "+ruePlace,
+                                      coordinate: CLLocationCoordinate2D(latitude: Double(latitude), longitude: Double(longitude)))
+                
+                //Ajout du pin
+                mapView.addAnnotation(artwork)
+        }
     }
 
     override func didReceiveMemoryWarning() {
